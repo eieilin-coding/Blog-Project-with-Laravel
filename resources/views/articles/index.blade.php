5071126a87a73a0d900ee28cfae94d0bcdd28d57
Eieilin <Eieilin2412@gmail.com>
2025-05-24 13:55:07 +0630

    Blog Project using PHP with Laravel framework

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,22 @@
+@extends("layouts.app")
+
+@section("content")
+
+    <div class="container" style="max-width: 800px">
+        @foreach ($articles as $article)
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h4>{{ $article->title}}</h4>
+                    <div class="text-muted">
+                        {{ $article->created_at}}
+                    </div>
+                    <p>
+                        {{ $article->body}}
+                    </p>
+                    <a href="{{ url("/articles/detail/$article->id") }}">View Detail</a>
+                </div>
+            </div>          
+        @endforeach
+    </div>
+
+@endsection
